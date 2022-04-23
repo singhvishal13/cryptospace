fb24453d639bc22573778047c22cae68bd8baf59
Singhvishal13 <Singhvishal1918@gmail.com>
2022-04-23 14:04:17 +0530

    currency and news

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 //import created app
 import App from './App';
 
-//passing app as parameter to root div
+//passing the app as parameter to root div
 ReactDOM.render(
     <Router>
         <App />
