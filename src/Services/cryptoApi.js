41644cec809564d65e7fef9b5007331c8954b2ab
Singhvishal13 <Singhvishal1918@gmail.com>
2022-05-30 14:46:40 +0530

    commit

--- src/Services/cryptoApi.js
+++ src/Services/cryptoApi.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-      'X-RapidAPI-Key': '8ab12e0260mshd3f448e2c4c7072p1429d3jsn6aa450dc2018'
+      // 'X-RapidAPI-Key': '8ab12e0260mshd3f448e2c4c7072p1429d3jsn6aa450dc2018'
+      'X-RapidAPI-Key': '0c176c27bdmsh24cd4d9b2ae3c05p11c4e0jsn57b3206836d4'
 };
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
--- src/Services/cryptoNewsApi.js
+++ src/Services/cryptoNewsApi.js
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
-  'X-BingApis-SDK': 'true',
-  'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
-  'X-RapidAPI-Key': '8ab12e0260mshd3f448e2c4c7072p1429d3jsn6aa450dc2018'
+  // 'X-BingApis-SDK': 'true',
+  // 'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+  // 'X-RapidAPI-Key': '8ab12e0260mshd3f448e2c4c7072p1429d3jsn6aa450dc2018'
+  'X-RapidAPI-Key': '0c176c27bdmsh24cd4d9b2ae3c05p11c4e0jsn57b3206836d4'
 }
 
 const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
